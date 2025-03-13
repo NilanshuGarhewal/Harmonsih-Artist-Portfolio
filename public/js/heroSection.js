544a3636f94bc9522ca6f9d7b1417d4b72e2bf3d
Nilanshu Garhewal <nilanshugarhewal1@gmail.com>
2025-03-13 12:35:49 +0530

    update some necessary changes in the file

---
+++ public/js/heroSection.js
@@ -0,0 +1,16 @@
+// gsap.registerPlugin(ScrollTrigger);
+
+// gsap.to(".animate-text", {
+//   scrollTrigger: {
+//     trigger: ".animate-text",
+//     start: "top 70%",
+//     end: "bottom 200px",
+//     markers: true,
+//     scrub: true,
+//     pin: ".c",
+//     toggleActions: "restart pause reverse none",
+//   },
+//   x: 500,
+//   //   rotation: 360,
+//   duration: 3,
+// });
