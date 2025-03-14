99c66db93fef0eb7d53e9d2a7c9ee440b0dfb517
Nilanshu Garhewal <nilanshugarhewal1@gmail.com>
2025-03-14 22:47:43 +0530

    created the faq section & updated the about section

--- public/js/aboutSection.js
+++ public/js/aboutSection.js
@@ -14,9 +14,37 @@ gsap.from(".word", {
   stagger: 0.5,
 });
 
+// const projectInfo = new SplitType(".project-info", { types: "words" });
+
+// gsap.from(".word", {
+//   scrollTrigger: {
+//     trigger: ".word",
+//     start: "top 70%",
+//     end: "top 20%",
+//     scrub: true,
+//     markers: false,
+//   },
+//   opacity: 0.2,
+//   stagger: 0.5,
+// });
+
 gsap.to(".side-slide-animation", {
   scrollTrigger: {
     trigger: ".side-slide-animation",
+    toggleActions: "restart pause reverse pause",
+    start: "20% 80%",
+    end: "bottom 40%",
+    scrub: true,
+    markers: false,
+  },
+  x: 0,
+  opacity: 1,
+  duration: 2,
+});
+
+gsap.to(".side-slide-animation-3", {
+  scrollTrigger: {
+    trigger: ".side-slide-animation-3",
     toggleActions: "restart pause reverse pause",
     start: "20% 80%",
     end: "bottom 40%",
